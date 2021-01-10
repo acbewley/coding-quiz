4b3cbe3c6c69da923d064e34ffda7503cc1feb43
acbewley <acbewley13@gmail.com>
2021-01-10 15:15:45 -0500

    changed question array format

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,45 @@ var answer1 = document.querySelector("#answer-1");
 var answer2 = document.querySelector("#answer-2");
 var answer3 = document.querySelector("#answer-3");
 var answer4 = document.querySelector("#answer-4");
-var ansButtons = document.querySelector("#answer");
-var question1 = {
-    question: "What coding language is used to create the basic appearance of a webpage?",
-    answer1: "CSS",
-    answer2: "HTML",
-    answer3: "Javascript",
-    answer4: "Python"
-};
+var questions = [
+    {
+        question: "What coding language is used to create the basic appearance of a webpage?",
+        answers: {
+            a: "CSS",
+            b: "HTML",
+            c: "Javascript",
+            d: "Python"
+        },
+        correct: "b",
+    },
+    {
+        question: "What coding laguage is used to change and sylize the look of an HTML page?",
+        answers: {
+            a: "CSS",
+            b: "HTML",
+            c: "Javascript",
+            d: "Python"
+        },
+        correct: "a",
+    },
+    {
+        question: "What coding language is used to create the behind the scenes functions of a webpage, such as button clicks and animation?",
+        answers: {
+            a: "CSS",
+            b: "HTML",
+            c: "Javascript",
+            d: "Python"
+        },
+    }
+    ];
 
-startButton.addEventListener("click", function () {
-    question.textContent = question1.question;
-    ansButtons.setAttribute("class", "visible");
-});
+// startButton.addEventListener("click", function () {
+//     question.textContent = question1.question;
+//     answer2.setAttribute("class", "visible btn btn-info")
+//     answer3.setAttribute("class", "visible btn btn-info")
+//     answer4.setAttribute("class", "visible btn btn-info")
+//     answer1.innerHTML = question1.answer1
+//     answer2.innerHTML = question1.answer2
+//     answer3.innerHTML = question1.answer3
+//     answer4.innerHTML = question1.answer4
+// });
