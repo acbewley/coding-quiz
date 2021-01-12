6eae965673aa14b9c810ffdee822279eecbc0b3e
acbewley <acbewley13@gmail.com>
2021-01-12 18:21:36 -0500

    Fixed question selection and added answer button event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,116 @@
 var startButton = document.querySelector("#start-button");
 var introCard = document.querySelector("#intro")
 var questionCard = document.querySelector("#question");
+var questionText = document.querySelector("#question-text")
+var answers = document.querySelectorAll("#answers")
 var answer1 = document.querySelector("#answer-1");
 var answer2 = document.querySelector("#answer-2");
 var answer3 = document.querySelector("#answer-3");
 var answer4 = document.querySelector("#answer-4");
-var timer = document.querySelector("#timer")
+var timer = document.querySelector("#timer");
+var rightWrong = document.querySelector("#right-wrong")
+var currentQuestion = 0
 var questions = [
     {
-        question: "What coding language is used to create the basic appearance of a webpage?",
-        answers: {
-            a: "CSS",
-            b: "HTML",
-            c: "Javascript",
-            d: "Python"
+        question: "what the heck",
+        answer: {
+            a: "yep",
+            b: "nope",
+            c: "maybe",
+            d: "whatever, man"
         },
-        correct: "b",
+        correct: "nope"
     },
     {
-        question: "What coding laguage is used to change and sylize the look of an HTML page?",
-        answers: {
-            a: "CSS",
-            b: "HTML",
-            c: "Javascript",
-            d: "Python"
+        question: "how the heck?",
+        answer: {
+            a: "idk",
+            b: "crazy",
+            c: "wow",
+            d: "hooo"
         },
-        correct: "a",
+        correct: "hooo"
     },
     {
-        question: "What coding language is used to create the behind the scenes functions of a webpage, such as button clicks and animation?",
-        answers: {
-            a: "CSS",
-            b: "HTML",
-            c: "Javascript",
-            d: "Python"
+        question: "when the heck",
+        answer: {
+            a: "then",
+            b: "now",
+            c: "never",
+            d: "forever"
         },
+        correct: "never"
     }
-];
+]
 
-startButton.addEventListener("click", function() {
+function getQuestions() {
+    questionText.textContent = questions[currentQuestion].question
+    answer1.textContent = questions[currentQuestion].answer.a
+    answer2.textContent = questions[currentQuestion].answer.b
+    answer3.textContent = questions[currentQuestion].answer.c
+    answer4.textContent = questions[currentQuestion].answer.d
+};
+
+startButton.addEventListener("click", function () {
     introCard.setAttribute("class", "d-none");
     questionCard.setAttribute("class", "card");
+    getQuestions();
+    console.log(questions.length)
 });
 
+answer1.addEventListener("click", function () {
+    if (answer1.textContent === questions[currentQuestion].correct) {
+        rightWrong.textContent = "Correct!";
+    } else if (answer1.textContent != questions[currentQuestion].correct) {
+        rightWrong.textContent = "Wrong!";
+    }
+    if (currentQuestion < (questions.length - 1)) {
+        currentQuestion++;
+        getQuestions();
+    } else {
+        console.log("done")
+    }
+
+})
+
+answer2.addEventListener("click", function () {
+    if (answer2.textContent === questions[currentQuestion].correct) {
+        rightWrong.textContent = "Correct!";
+    } else if (answer2.textContent != questions[currentQuestion].correct) {
+        rightWrong.textContent = "Wrong!";
+    }
+    if (currentQuestion < (questions.length - 1)) {
+        currentQuestion++;
+        getQuestions();
+    } else {
+        console.log("done")
+    }
+})
+
+answer3.addEventListener("click", function () {
+    if (answer3.textContent === questions[currentQuestion].correct) {
+        rightWrong.textContent = "Correct!";
+    } else if (answer3.textContent != questions[currentQuestion].correct) {
+        rightWrong.textContent = "Wrong!";
+    }
+    if (currentQuestion < (questions.length - 1)) {
+        currentQuestion++;
+        getQuestions();
+    } else {
+        console.log("done")
+    }
+})
+
+answer4.addEventListener("click", function () {
+    if (answer4.textContent === questions[currentQuestion].correct) {
+        rightWrong.textContent = "Correct!";
+    } else if (answer4.textContent != questions[currentQuestion].correct) {
+        rightWrong.textContent = "Wrong!";
+    }
+    if (currentQuestion < (questions.length - 1)) {
+        currentQuestion++;
+        getQuestions();
+    } else {
+        console.log("done")
+    }
+})
